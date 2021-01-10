173371a5dba28d73a8db5746715ba5afed37232a
n8creator <n8creator@pm.me>
2021-01-10 20:07:56 +0300

    S2: saver.py Renamed to file.py

--- page_loader/file.py
+++ page_loader/file.py
@@ -13,3 +13,17 @@ def save_file(data, local_path):
             file.write(data)
     except Exception as err:
         print('An error occurred while saving the file:' + str(err))
+
+
+def read_file(local_path):
+    """Read file data and store into variable.
+
+    Args:
+        local_path ([str]): local path to file
+    """
+    try:
+        with open(local_path, 'r') as file:
+            data = file.read()
+        return data
+    except Exception as err:
+        print('An error occurred while reading the file:' + str(err))
