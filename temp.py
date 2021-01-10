abe7dea48eb3289d3311b5a16c33033a8d54b0a2
n8creator <n8creator@pm.me>
2021-01-10 16:35:31 +0300

    S2: Fixtures Updated to Short Names

--- temp.py
+++ temp.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 
-with open('ru-hexlet-io-professions.html', 'r') as f:
+with open('tests/fixtures/ru-hexlet-io-professions.html', 'r') as f:
     data = f.read()
 
 
 def get_links(data):
     soup = BeautifulSoup(data, 'lxml')
-    imgs = soup.find_all('link')
+    # imgs = soup.find_all('link')
+    imgs = soup("img")
+    output = []
+    for img in imgs:
+        src = img.get('src')
+        output.append(src)
+    return output
     return imgs
 
 
