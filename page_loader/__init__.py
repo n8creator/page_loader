60384bba47c210bb1862ba705af7ee9e33f70208
n8creator <n8creator@pm.me>
2021-10-20 14:16:02 +0300

    Commentary to mentor added

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -16,7 +16,6 @@ DEFAULT_PATH = os.getcwd()
 
 def download(url, path=DEFAULT_PATH):
     """Download HTML page and page assets (img, css files) from given 'url'."""
-
     # Generate output 'page_name' and 'file_path' and load page
     page_name = get_filename(url=url, ext='html')
     file_path = get_full_path(path, page_name)
@@ -28,17 +27,33 @@ def download(url, path=DEFAULT_PATH):
     # Get list of links
     links = get_links(tag_meta=ASSET_TAGS, url=url, soup=soup)
 
+    # Edit Soup object and replace links to loclal files
     if links:
         # Generate folder_name and create directory
         folder_name = get_foldername(url=url)
         create_dir(local_path=get_full_path(path, folder_name))
 
-        # Initiate download queue
-        to_download = []
+        to_download = []  # Initiate download queue
 
         # Iterate links and edit Soup object
         for link, tag in links:
             # Generate 'absolute_url' to load and 'file_name' for item
+
+            # ################# КОММЕНТАРИЙ МЕНТОРУ START #################
+
+            # Я осознанно оставил вызов функции get_abs_link(), хотя в
+            # в замечаниях Вы предложили возвращать абсолютные пути
+            # непосредственно при вызове функции get_links() (28 строка выше).
+            # Проблема в том, что при редактировании HTML-страницы нам нужно
+            # менять ФАКТИЧЕСКИЕ URL, которые встречаются на странице -
+            # а они могуть быть как абсолютными, так и относительными.
+
+            # Поэтому для редактирования HTML-страницы мы получаем с помощью
+            # функции get_links() фактически встречающиеся URL, а для
+            # скачивания ресурсов преобразуем их в абсолютные...
+
+            # ################# КОММЕНТАРИЙ МЕНТОРУ END #################
+
             absolute_url = get_abs_link(page_url=url, local_link=link)
             ext = itemgetter('ext')(split_path_and_ext(link))
             file_name = get_filename(url=absolute_url, ext=ext)
@@ -55,22 +70,16 @@ def download(url, path=DEFAULT_PATH):
     # Save modified soup
     save_file(data=soup.prettify(), local_path=file_path, mode='w')
 
-    # Initiate progress bar
+    # Initiate progress bar and download assets
     progress_bar = IncrementalBar('Loading resourses:', max=len(to_download))
-
-    # Download assets
     for absolute_url, local_path in to_download:
         try:
             content = make_request(absolute_url)
             save_file(data=content, local_path=local_path)
         except Exception:
             logger.error(f'Asset \'{link}\' was not downloaded.')
+        progress_bar.next()  # Iterate progress bar
 
-        # Start progress bar
-        progress_bar.next()
-
-    # Finish progess_bar
+    # Finish progess_bar & return output's file path
     progress_bar.finish()
-
-    # Return output file local path
     return file_path
