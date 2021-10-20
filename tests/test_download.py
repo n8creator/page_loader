fd31c0484223758551a6edc84e365b1908cad896
n8creator <n8creator@pm.me>
2021-10-20 13:50:52 +0300

    test_download() refactored to check if assets content

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,37 +1,46 @@
 import os
 import pytest
+import requests
 import tempfile
 import requests_mock
 from pathlib import PurePath as PP
 from page_loader import download
 from page_loader.file import read_file
-from page_loader.links import get_absolute_link
+from page_loader.links import get_abs_link
 
 
 URL = 'https://ru.hexlet.io/professions'
 FIXTURES_PATH = PP('tests/fixtures/')
 
 INPUT_FIXTURE = PP('inputs/ru-hexlet-io-professions.html')
-INPUT_ASSETS = [  # (asset_hyperlink, asset_local_path)
-    ('assets/frontend.png', PP('inputs/assets/frontend.png')),
-    ('assets/python.png', PP('inputs/assets/python.png')),
-    ('favicon.ico', PP('inputs/favicon.ico')),
-    ('assets/application.css', PP('inputs/assets/application.css')),
-    ('https://ru.hexlet.io/professions', PP('inputs/ru-hexlet-io-professions.html')),  ## noqa
-    ('assets/application.js', PP('inputs/assets/application.js')),
-    ('/', PP('inputs/ru-hexlet-io-professions.html')),
-]
-
 EXPECTED_FIXTURE = PP('expected/ru-hexlet-io-professions.html')
 EXPECTED_FILENAME = PP('ru-hexlet-io-professions.html')
-EXPECTED_ASSETS = [
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-frontend.png'),
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-python.png'),
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-favicon.ico'),
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-application.css'),
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-professions.html'),
-    PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-application.js'),
-    PP('ru-hexlet-io-professions.html'),
+
+ASSETS = [  # (link, local_path, expected_file)
+    ('assets/frontend.png',
+     PP('inputs/assets/frontend.png'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-frontend.png'),
+     ),
+    ('assets/python.png',
+     PP('inputs/assets/python.png'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-python.png'),
+     ),
+    ('favicon.ico',
+     PP('inputs/favicon.ico'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-favicon.ico'),
+     ),
+    ('assets/application.css',
+     PP('inputs/assets/application.css'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-application.css'),
+     ),
+    ('https://ru.hexlet.io/professions',
+     PP('inputs/ru-hexlet-io-professions.html'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-professions.html'),
+     ),
+    ('assets/application.js',
+     PP('inputs/assets/application.js'),
+     PP('ru-hexlet-io-professions_files/ru-hexlet-io-assets-application.js'),
+     ),
 ]
 
 
@@ -43,25 +52,30 @@ def test_download():
             mock.get(URL, text=html)
 
             # create mock's for all assets
-            for hyperlink, local_path in INPUT_ASSETS:
-                asset_hyperlink = get_absolute_link(
-                    page_url=URL, local_link=hyperlink)
+            for link, local_path, _ in ASSETS:
+                asset_link = get_abs_link(page_url=URL, local_link=link)
 
                 asset_fixture_path = os.path.join(FIXTURES_PATH, local_path)
                 bytecode = read_file(file_path=asset_fixture_path, mode='rb')
-                mock.get(asset_hyperlink, content=bytecode)
+                mock.get(asset_link, content=bytecode)
 
             # download page from URL
             download(URL, temp_dir)
 
-            # assert if downloaded html in 'tmp' folder equals expected in fixtures
+            # assert if downloaded html in temp folder equals expected in fixtures
             expected = read_file(os.path.join(FIXTURES_PATH, EXPECTED_FIXTURE))
             downloaded = read_file(os.path.join(temp_dir, EXPECTED_FILENAME))
             assert expected == downloaded
 
             # assert if expected assets exists in temp folder
-            for asset in EXPECTED_ASSETS:
-                assert os.path.exists(f'{temp_dir}/{asset}') is True
+            for _, _, expected_asset in ASSETS:
+                assert os.path.exists(f'{temp_dir}/{expected_asset}') is True
+
+            # assert if asset's content equals to asset's fixture
+            for link, _, expected_asset in ASSETS:
+                asset_link = get_abs_link(page_url=URL, local_link=link)
+                assert requests.get(asset_link).content == read_file(
+                    os.path.join(temp_dir, expected_asset), mode='rb')
 
 
 @pytest.mark.parametrize('status_code', [
